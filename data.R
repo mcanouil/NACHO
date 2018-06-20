f79d7c28ebaaa69e055dfe1563ca49af176cc06f
Gerard Bouland <G.Bouland@lumc.nl>
2018-06-20 13:55:35 +0200

    added vignette and package directory

---
+++ data.R
@@ -0,0 +1,7 @@
+#' Presummarized example data.
+#'
+#' A dataset containing sample specific attributes and .
+#'
+#' @format A list containing 11 items
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/}
+"exampleData"
