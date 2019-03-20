1bacd52a49fb2e557ea561144ab0d79a322a1ae2
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-03-20 15:46:59 +0100

    hex logo

---
+++ inst/logo/nacho_hex.R
@@ -0,0 +1,15 @@
+# devtools::install_github("GuangchuangYu/hexSticker")
+hexSticker::sticker(
+  subplot = "Nacho_logo_notext.png",
+  package = "NACHO",
+  p_size = 16,
+  p_y = 0.92,
+  p_color = "black", 
+  s_x = 1, 
+  s_y = 1,
+  s_width = 0.75, 
+  s_height = 0.75,
+  h_fill = "goldenrod2",
+  h_color = "goldenrod3",
+  filename = "nacho_hex.png"
+)
