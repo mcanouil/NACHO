deaa3bda72672b2ac3d3c9a4539b012d79980f3e
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-03-26 13:58:17 +0100

    add new tests

--- tests/testthat/test-normalise.R
+++ tests/testthat/test-normalise.R
@@ -7,7 +7,18 @@ test_that("default settings", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings", {
+test_that("missing nacho", {
+  expect_error(normalise())
+})
+
+test_that("missing field", {
+  GSE74821$nacho <- NULL
+  expect_error(normalise(GSE74821))
+})
+
+
+test_that("No POS_E", {
+  GSE74821$nacho <- GSE74821$nacho[GSE74821$nacho$Name!="POS_E(0.5)", ]
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -28,7 +39,7 @@ test_that("hard written settings", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: enes not null", {
+test_that("genes not null", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = c("RPLP0", "ACTB"),
@@ -49,7 +60,7 @@ test_that("hard written settings: enes not null", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: predict TRUE", {
+test_that("predict TRUE", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -70,7 +81,7 @@ test_that("hard written settings: predict TRUE", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: norm TRUE", {
+test_that("norm TRUE", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -91,7 +102,7 @@ test_that("hard written settings: norm TRUE", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: method GLM", {
+test_that("method GLM", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -112,7 +123,7 @@ test_that("hard written settings: method GLM", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: n_comp 2", {
+test_that("n_comp 2", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -133,7 +144,7 @@ test_that("hard written settings: n_comp 2", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: n_comp 10", {
+test_that("n_comp 10", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -154,7 +165,7 @@ test_that("hard written settings: n_comp 10", {
   expect_identical(class(res), "list")
 })
 
-test_that("hard written settings: outliers TRUE", {
+test_that("outliers TRUE", {
   res <- normalise(
     nacho_object = GSE74821,
     housekeeping_genes = NULL,
@@ -192,3 +203,92 @@ test_that("Test outliers", {
   expect_identical(class(res), "list")
 })
 
+
+test_that("All LoD to zero", {
+  GSE74821$nacho$LoD <- 0
+  res <- normalise(
+    nacho_object = GSE74821,
+    housekeeping_genes = c("RPLP0", "ACTB"),
+    housekeeping_predict = FALSE,
+    housekeeping_norm = FALSE,
+    normalisation_method = "GEO",
+    n_comp = 10,
+    remove_outliers = FALSE,
+    outliers_thresholds = list(
+      BD = c(0.1, 2.25),
+      FoV = 75,
+      LoD = 2,
+      PC = 0.95,
+      Positive_factor = c(1/4, 4),
+      House_factor = c(1/11, 11)
+    )
+  )
+  expect_identical(class(res), "list")
+})
+
+test_that("All PC to zero", {
+  GSE74821$nacho$PC <- 0
+  res <- normalise(
+    nacho_object = GSE74821,
+    housekeeping_genes = c("RPLP0", "ACTB"),
+    housekeeping_predict = FALSE,
+    housekeeping_norm = FALSE,
+    normalisation_method = "GEO",
+    n_comp = 10,
+    remove_outliers = FALSE,
+    outliers_thresholds = list(
+      BD = c(0.1, 2.25),
+      FoV = 75,
+      LoD = 2,
+      PC = 0.95,
+      Positive_factor = c(1/4, 4),
+      House_factor = c(1/11, 11)
+    )
+  )
+  expect_identical(class(res), "list")
+})
+
+test_that("housekeeping_norm to FALSE and remove_outliers to TRUE", {
+  GSE74821$nacho$PC <- 0
+  res <- normalise(
+    nacho_object = GSE74821,
+    housekeeping_genes = NULL,
+    housekeeping_predict = FALSE,
+    housekeeping_norm = FALSE,
+    normalisation_method = "GEO",
+    n_comp = 10,
+    remove_outliers = TRUE,
+    outliers_thresholds = list(
+      BD = c(0.1, 2.25),
+      FoV = 75,
+      LoD = 2,
+      PC = 0.95,
+      Positive_factor = c(1/4, 4),
+      House_factor = c(1/11, 11)
+    )
+  )
+  expect_identical(class(res), "list")
+})
+
+
+test_that("housekeeping_norm to TRUE and remove_outliers to TRUE", {
+  GSE74821$nacho$PC <- 0
+  res <- normalise(
+    nacho_object = GSE74821,
+    housekeeping_genes = NULL,
+    housekeeping_predict = FALSE,
+    housekeeping_norm = TRUE,
+    normalisation_method = "GEO",
+    n_comp = 10,
+    remove_outliers = TRUE,
+    outliers_thresholds = list(
+      BD = c(0.1, 2.25),
+      FoV = 75,
+      LoD = 2,
+      PC = 0.95,
+      Positive_factor = c(1/4, 4),
+      House_factor = c(1/11, 11)
+    )
+  )
+  expect_identical(class(res), "list")
+})
