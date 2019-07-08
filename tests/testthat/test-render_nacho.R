febf053d96be18244226c401d0d3bfdfdebe6811
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-08 17:52:14 +0200

    change expect condition for render_nacho unit tests

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -1,14 +1,14 @@
 context("render_nacho()")
 
 test_that("Default parameters", {
-  testthat::expect_success(render_nacho(nacho_object = GSE74821))
+  testthat::expect_message(render_nacho(nacho_object = GSE74821), "Output created: NACHO_QC.html")
 })
 
 
 test_that("with legend", {
-  testthat::expect_success(render_nacho(nacho_object = GSE74821, legend = TRUE))
+  testthat::expect_message(render_nacho(nacho_object = GSE74821, legend = TRUE), "Output created: NACHO_QC.html")
 })
 
 test_that("with Rmd", {
-  testthat::expect_success(render_nacho(nacho_object = GSE74821, keep_rmd = TRUE))
+  testthat::expect_message(render_nacho(nacho_object = GSE74821, keep_rmd = TRUE), "Output created: NACHO_QC.html")
 })
