ebaf6416d4ab3d6c8a56ec196ed8d1853881f87c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-08 17:44:39 +0200

    add unit test with legend or keep_rmd to TRUE

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -3,3 +3,12 @@ context("render_nacho()")
 test_that("Default parameters", {
   testthat::expect_success(render_nacho(nacho_object = GSE74821))
 })
+
+
+test_that("with legend", {
+  testthat::expect_success(render_nacho(nacho_object = GSE74821, legend = TRUE))
+})
+
+test_that("with Rmd", {
+  testthat::expect_success(render_nacho(nacho_object = GSE74821, keep_rmd = TRUE))
+})
