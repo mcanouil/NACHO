6785dee536fbb8e9404e95347175c3aa46e55379
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-08 17:52:33 +0200

    remove blank line

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -4,7 +4,6 @@ test_that("Default parameters", {
   testthat::expect_message(render_nacho(nacho_object = GSE74821), "Output created: NACHO_QC.html")
 })
 
-
 test_that("with legend", {
   testthat::expect_message(render_nacho(nacho_object = GSE74821, legend = TRUE), "Output created: NACHO_QC.html")
 })
