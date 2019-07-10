6342115805bfaf38489aee55aea8a8797c3ae560
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-10 11:53:09 +0200

    explicit output to tmp and shorten regexp

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -2,21 +2,21 @@ context("render_nacho()")
 
 test_that("Default parameters", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = NULL),
-    regexp = "Output created: NACHO_QC.html"
+    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp"),
+    regexp = "Output created"
   )
 })
 
 test_that("with legend", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = NULL, legend = TRUE),
-    regexp = "Output created: NACHO_QC.html"
+    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp", legend = TRUE),
+    regexp = "Output created"
   )
 })
 
 test_that("with Rmd", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = NULL, keep_rmd = TRUE),
-    regexp = "Output created: NACHO_QC.html"
+    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp", keep_rmd = TRUE),
+    regexp = "Output created"
   )
 })
