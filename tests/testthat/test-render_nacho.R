c404f8d931755c87d5847e151b2eb3a7c9056596
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-10 10:13:39 +0200

    change default output directory

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -1,13 +1,13 @@
 context("render_nacho()")
 
 test_that("Default parameters", {
-  testthat::expect_message(render_nacho(nacho_object = GSE74821), "Output created: NACHO_QC.html")
+  testthat::expect_message(render_nacho(nacho_object = GSE74821, output_dir = NULL), "Output created: NACHO_QC.html")
 })
 
 test_that("with legend", {
-  testthat::expect_message(render_nacho(nacho_object = GSE74821, legend = TRUE), "Output created: NACHO_QC.html")
+  testthat::expect_message(render_nacho(nacho_object = GSE74821, output_dir = NULL, legend = TRUE), "Output created: NACHO_QC.html")
 })
 
 test_that("with Rmd", {
-  testthat::expect_message(render_nacho(nacho_object = GSE74821, keep_rmd = TRUE), "Output created: NACHO_QC.html")
+  testthat::expect_message(render_nacho(nacho_object = GSE74821, output_dir = NULL, keep_rmd = TRUE), "Output created: NACHO_QC.html")
 })
