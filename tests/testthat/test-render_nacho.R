931383489eff9fb0b326435fc6277bc62cd2200e
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-10 12:04:43 +0200

    try to fix appveyor CI withtout output directory to NULL

--- tests/testthat/test-render_nacho.R
+++ tests/testthat/test-render_nacho.R
@@ -2,21 +2,21 @@ context("render_nacho()")
 
 test_that("Default parameters", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp"),
+    object = render_nacho(nacho_object = GSE74821, output_dir = NULL),
     regexp = "Output created"
   )
 })
 
 test_that("with legend", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp", legend = TRUE),
+    object = render_nacho(nacho_object = GSE74821, output_dir = NULL, legend = TRUE),
     regexp = "Output created"
   )
 })
 
 test_that("with Rmd", {
   testthat::expect_message(
-    object = render_nacho(nacho_object = GSE74821, output_dir = "/tmp", keep_rmd = TRUE),
+    object = render_nacho(nacho_object = GSE74821, output_dir = NULL, keep_rmd = TRUE),
     regexp = "Output created"
   )
 })
