16736e4ed7374f38e576345287281c3109f30149
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-12 13:02:14 +0200

    change to to make coverage test working

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -2,7 +2,7 @@ context("render()")
 
 test_that("Default parameters", {
   expect_true(
-    object = render(nacho_object = GSE74821, output_dir = NULL)
+    render(nacho_object = GSE74821, output_dir = NULL)
   )
 })
 
@@ -15,5 +15,20 @@ test_that("with legend", {
 test_that("with Rmd", {
   expect_true(
     render(nacho_object = GSE74821, output_dir = NULL, keep_rmd = TRUE)
+  )
+})
+
+
+context("print_nacho()")
+
+test_that("default", {
+  expect_null(
+    print_nacho(GSE74821, colour = "CartridgeID", size = 0.5, show_legend = FALSE)
+  )
+})
+
+test_that("show_legend to TRUE", {
+  expect_null(
+    print_nacho(GSE74821, colour = "CartridgeID", size = 0.5, show_legend = TRUE)
   )
 })
