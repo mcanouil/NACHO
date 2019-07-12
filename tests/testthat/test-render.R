a146787732fe0469ec260d0e7f31bfcf9d48c138
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-12 15:23:36 +0200

    replace expect treu to null

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,19 +1,19 @@
 context("render()")
 
 test_that("Default parameters", {
-  expect_true(
+  expect_null(
     render(nacho_object = GSE74821, output_dir = NULL)
   )
 })
 
 test_that("with legend", {
-  expect_true(
+  expect_null(
     render(nacho_object = GSE74821, output_dir = NULL, show_legend = TRUE)
   )
 })
 
 test_that("with Rmd", {
-  expect_true(
+  expect_null(
     render(nacho_object = GSE74821, output_dir = NULL, keep_rmd = TRUE)
   )
 })
