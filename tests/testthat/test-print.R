a06127fd4e1c87635dece59601995dcef0824f41
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-16 16:49:27 +0200

    separate test for print

---
+++ tests/testthat/test-print.R
@@ -0,0 +1,13 @@
+context("print()")
+
+test_that("default", {
+  null <- capture.output(out <- suppressWarnings(print.nacho(GSE74821)))
+  expect_null(out)
+})
+
+test_that("show_legend to TRUE", {
+  null <- capture.output(out <- suppressWarnings(
+    print.nacho(GSE74821, colour = "CartridgeID", size = 0.5, show_legend = TRUE)
+  ))
+  expect_null(out)
+})
