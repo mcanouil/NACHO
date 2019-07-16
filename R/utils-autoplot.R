f3c4f3799beff8ddc9916ec9d8689d1f8dd22968
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-16 16:48:13 +0200

    use autoplot method instad of plot

---
+++ R/utils-autoplot.R
@@ -0,0 +1,10 @@
+#' autoplot generic
+#'
+#' See \code{ggplot2::\link[ggplot2]{autoplot}} for details.
+#'
+#' @name autoplot
+#' @rdname autoplot
+#' @keywords internal
+#' @export
+#' @importFrom ggplot2 autoplot
+NULL
