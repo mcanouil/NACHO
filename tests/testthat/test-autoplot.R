9b360ec75a1a9034fcec2827fee25cd218e0f7da
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-17 17:00:30 +0200

    fix test

--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -1,13 +1,9 @@
-test_that(paste(imetric, 'Missing "x"', sep = " - "), {
-  expect_error(
-    object = autoplot(object = GSE74821)
-  )
+test_that('Missing "x"', {
+  expect_error(autoplot(object = GSE74821))
 })
 
-test_that(paste(imetric, 'Wrong "x"', sep = " - "), {
-  expect_error(
-    object = autoplot(object = GSE74821, x = "FAKE_VALUE")
-  )
+test_that('Wrong "x"', {
+  expect_error(autoplot(object = GSE74821, x = "FAKE_VALUE"))
 })
 
 metrics <- c(
