3b41fe6ebd6d4b3dddc3230017d7e41292497351
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-18 17:38:48 +0200

    Fix missing +1 for count==0 in autoplot

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,11 +1,11 @@
 test_that("default", {
-  null <- capture.output(out <- suppressWarnings(print(GSE74821)))
+  null <- capture.output(out <- print(GSE74821))
   expect_null(out)
 })
 
 test_that("show_legend to TRUE", {
-  null <- capture.output(out <- suppressWarnings(
-    print(GSE74821, colour = "CartridgeID", size = 0.5, show_legend = TRUE)
-  ))
+  null <- capture.output(
+    out <- print(GSE74821, colour = "CartridgeID", size = 0.5, show_legend = TRUE)
+  )
   expect_null(out)
 })
--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -1,14 +1,14 @@
 test_that("Default parameters", {
-  out <- suppressWarnings(render(nacho_object = GSE74821))
+  out <- render(nacho_object = GSE74821)
   expect_null(out)
 })
 
 test_that("with legend", {
-  out <- suppressWarnings(render(nacho_object = GSE74821, show_legend = TRUE))
+  out <- render(nacho_object = GSE74821, show_legend = TRUE)
   expect_null(out)
 })
 
 test_that("with Rmd", {
-  out <- suppressWarnings(render(nacho_object = GSE74821, clean = FALSE))
+  out <- render(nacho_object = GSE74821, clean = FALSE)
   expect_null(out)
 })
