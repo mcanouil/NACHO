7431e96dd153cb7cb8e4f4769b2376304323883d
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-12-16 15:22:16 +0100

    card style for plot

--- inst/app/app.R
+++ inst/app/app.R
@@ -8,107 +8,91 @@ library(NACHO)
 
 source("utils.R")
 
-ui <- tagList(
-  tags$head(tags$style(HTML(
-    ".navbar-nav { float: none !important; } .navbar-nav > li:nth-child(6) { float: right; }"
-  ))),
-  navbarPage(
-    theme = "united-bootstrap.min.css",
-    title = span(img(src = "nacho_hex.png", height = 18), "NACHO"),
-    windowTitle = "NACHO",
-    collapsible = TRUE,
-    # footer = div(
-    #   p(
-    #     img(src = "www/nacho_hex.png", height = 18),
-    #     "Full documentation on", icon("github"), "at",
-    #     a(url = "https://mcanouil.github.io/NACHO", "mcanouil.github.io/NACHO")
-    #   ),
-    #   align = "center",
-    #   style = "margin: 5px;"
-    # ),
-    panelInputUI("qc_metrics", "QC Metrics",
-      sidebar = {
-        list(
-          h3("QC Thresholds"),
-          radioButtons("qc_bd_metrics", span("Binding Density", actionLink("about_bd", NULL, icon = icon("info-circle"))),
-            choiceNames = list(
-              span("MAX/FLEX", helpText("(Default: 0.1 - 2.25)")),
-              span("SPRINT", helpText("(Default: 0.1 - 1.8)"))
-            ),
-            choiceValues = list("MAX/FLEX", "SPRINT"),
-            inline = TRUE
+ui <- navbarPage(
+  theme = "united-bootstrap.min.css",
+  title = span(img(src = "nacho_hex.png", height = 18), "NACHO"),
+  windowTitle = "NACHO",
+  collapsible = TRUE,
+  id = "main-menu",
+  # footer = div(
+  #   p(
+  #     img(src = "www/nacho_hex.png", height = 18),
+  #     "Full documentation on", icon("github"), "at",
+  #     a(url = "https://mcanouil.github.io/NACHO", "mcanouil.github.io/NACHO")
+  #   ),
+  #   align = "center",
+  #   style = "margin: 5px;"
+  # ),
+  panelInputUI("qc_metrics", "QC Metrics",
+    sidebar = {
+      list(
+        h3("QC Thresholds"),
+        radioButtons("qc_bd_metrics", span("Binding Density", actionLink("about_bd", NULL, icon = icon("info-circle"))),
+          choiceNames = list(
+            span("MAX/FLEX", helpText("(Default: 0.1 - 2.25)")),
+            span("SPRINT", helpText("(Default: 0.1 - 1.8)"))
           ),
-          sliderInput("qc_bd_thresh", NULL,
-            min = 0.1, max = 2.25, value = c(0.1, 2.25)
-          ),
-          sliderInput("qc_fov_thresh", span("Field of View", actionLink("about_fov", NULL, icon = icon("info-circle")), helpText("(Default: 75)")),
-            min = 50, max = 100, value = 75
-          ),
-          sliderInput("qc_pcl_thresh", span("Positive Control Linearity", actionLink("about_pcl", NULL, icon = icon("info-circle")), helpText("(Default: 0.95)")),
-            min = 0.5, max = 1, value = 0.95
-          ),
-          sliderInput("qc_lod_thresh", span("Limit of Detection", actionLink("about_lod", NULL, icon = icon("info-circle")), helpText("(Default: 2)")),
-            min = 0, max = 30, value = 2
-          )
+          choiceValues = list("MAX/FLEX", "SPRINT"),
+          inline = TRUE
+        ),
+        sliderInput("qc_bd_thresh", NULL,
+          min = 0.1, max = 2.25, value = c(0.1, 2.25)
+        ),
+        sliderInput("qc_fov_thresh", span("Field of View", actionLink("about_fov", NULL, icon = icon("info-circle")), helpText("(Default: 75)")),
+          min = 50, max = 100, value = 75
+        ),
+        sliderInput("qc_pcl_thresh", span("Positive Control Linearity", actionLink("about_pcl", NULL, icon = icon("info-circle")), helpText("(Default: 0.95)")),
+          min = 0.5, max = 1, value = 0.95
+        ),
+        sliderInput("qc_lod_thresh", span("Limit of Detection", actionLink("about_lod", NULL, icon = icon("info-circle")), helpText("(Default: 2)")),
+          min = 0, max = 30, value = 2
         )
-      },
-      plotInputUI("Binding Density"),
-      plotInputUI("Field of View", right = TRUE),
-      plotInputUI("Positive Control Linearity"),
-      plotInputUI("Limit of Detection", right = TRUE)
-    ),
-    panelInputUI("qc_control", "Control Probes",
-      sidebar = NULL,
-      plotInputUI("Positive Probes"),
-      plotInputUI("Negative Probes", right = TRUE),
-      plotInputUI("Housekeeping Genes Probes"),
-      plotInputUI("Control Probe Expression", right = TRUE)
-    ),
-    panelInputUI("qc_count", "Counts",
-      sidebar = NULL,
-      plotInputUI("Average Count vs. Binding Density"),
-      plotInputUI("Average Count vs. Median Count", right = TRUE),
-      plotInputUI("Principal Component Analysis"),
-      plotInputUI("Principal Component Analysis Inertia", right = TRUE)
-    ),
-    panelInputUI("norm", "Normalisation",
-      sidebar = {
-        list(
-          sliderInput("qc_pf_thresh", span("Positive Factor", helpText("(Default: 0.25 - 4)")),
-            min = 0.25, max = 4, value = c(0.25, 4)
-          ),
-          sliderInput("qc_hgf_thresh", span("Housekeeping Genes Factor", helpText("(Default: 0.09 - 11)")),
-            min = 0.09, max = 11, value = c(0.09, 11)
-          )
+      )
+    },
+    plotInputUI("Binding Density"),
+    plotInputUI("Field of View", right = TRUE),
+    plotInputUI("Positive Control Linearity"),
+    plotInputUI("Limit of Detection", right = TRUE)
+  ),
+  panelInputUI("qc_control", "Control Probes",
+    sidebar = NULL,
+    plotInputUI("Positive Probes"),
+    plotInputUI("Negative Probes", right = TRUE),
+    plotInputUI("Housekeeping Genes Probes"),
+    plotInputUI("Control Probe Expression", right = TRUE)
+  ),
+  panelInputUI("qc_count", "Counts",
+    sidebar = NULL,
+    plotInputUI("Average Count vs. Binding Density"),
+    plotInputUI("Average Count vs. Median Count", right = TRUE),
+    plotInputUI("Principal Component Analysis"),
+    plotInputUI("Principal Component Analysis Inertia", right = TRUE)
+  ),
+  panelInputUI("norm", "Normalisation",
+    sidebar = {
+      list(
+        sliderInput("qc_pf_thresh", span("Positive Factor", helpText("(Default: 0.25 - 4)")),
+          min = 0.25, max = 4, value = c(0.25, 4)
+        ),
+        sliderInput("qc_hgf_thresh", span("Housekeeping Genes Factor", helpText("(Default: 0.09 - 11)")),
+          min = 0.09, max = 11, value = c(0.09, 11)
         )
-      },
-      plotInputUI("Positive Factor vs. Negative Factor"),
-      plotInputUI("Housekeeeping Genes Factor", right = TRUE),
-      plotInputUI("Normalisation Result", right = TRUE)
-    ),
-    panelInputUI("outliers", "Outliers",
-      tableOutput("outliers-server")
-    ),
-    tabPanel("About", icon = icon("info"), value = "about-tab", p(includeMarkdown("www/about-nacho.md")))
-))
+      )
+    },
+    plotInputUI("Positive Factor vs. Negative Factor"),
+    plotInputUI("Housekeeeping Genes Factor", right = TRUE),
+    plotInputUI("Normalisation Result", right = TRUE)
+  ),
+  tabPanel("About", icon = icon("info"), value = "about-tab",
+    p(includeMarkdown("www/about-nacho.md"))
+  )
+)
 
 server <- function(input, output, session) {
-  nacho_object <- reactive({ get(data(GSE74821, package = "NACHO")) })
-  nacho_custom <- reactive({
-    nacho_current <- nacho_object()
-    nacho_current$outliers_thresholds <- list(
-      BD = input$qc_bd_thresh %||% nacho_current$outliers_thresholds[["BD"]],
-      FoV = input$qc_fov_thresh %||% nacho_current$outliers_thresholds[["FoV"]],
-      LoD = input$qc_lod_thresh %||% nacho_current$outliers_thresholds[["LoD"]],
-      PCL = input$qc_pcl_thresh %||% nacho_current$outliers_thresholds[["PCL"]],
-      Positive_factor = nacho_current$outliers_thresholds[["Positive_factor"]],
-      House_factor = nacho_current$outliers_thresholds[["House_factor"]]
-    )
-    nacho_current
-  })
-
+  # ---------------------------------------- UI / SERVER
   # Global UI input
   observe({
+    nacho_tmp <- nacho_custom()
     map(
       .x = c(
         "bd", "fov", "pcl", "lod",
@@ -116,7 +100,7 @@ server <- function(input, output, session) {
         "acvbd", "acvmc", "pca", "pcai",
         "pfvnf", "hgf", "nr"
       ),
-      .f = ~plotInput(.x, nacho_custom())
+      .f = ~ plotInput(.x, nacho_tmp)
     )
   })
 
@@ -163,15 +147,53 @@ server <- function(input, output, session) {
     }
   )
 
-  output[["outliers-server"]] <- renderTable({
+  # ---------------------------------------- Input
+  # Get nacho object and update thresholds
+  nacho_custom <- reactive({
+    nacho_object <- get(data(GSE74821, package = "NACHO"))
+    nacho_object$outliers_thresholds[["BD"]] <- input$qc_bd_thresh %||%
+      nacho_object$outliers_thresholds[["BD"]]
+    nacho_object$outliers_thresholds[["FoV"]] <- input$qc_fov_thresh %||%
+      nacho_object$outliers_thresholds[["FoV"]]
+    nacho_object$outliers_thresholds[["LoD"]] <- input$qc_lod_thresh %||%
+      nacho_object$outliers_thresholds[["LoD"]]
+    nacho_object$outliers_thresholds[["PCL"]] <- input$qc_pcl_thresh %||%
+      nacho_object$outliers_thresholds[["PCL"]]
+    nacho_object$outliers_thresholds[["Positive_factor"]] <- input$qc_pf_thresh %||%
+      nacho_object$outliers_thresholds[["Positive_factor"]]
+    nacho_object$outliers_thresholds[["House_factor"]] <- input$qc_hgf_thresh %||%
+      nacho_object$outliers_thresholds[["House_factor"]]
+    nacho_object
+  })
+
+  # ---------------------------------------- Output
+  outliers_list <- reactive({
     ot <- nacho_custom()$outliers_thresholds
-    df <- distinct(nacho_custom()$nacho, sample_ID, CartridgeID, BD, FoV, PCL, LoD, MC, MedC)
+    df <- distinct(
+      nacho_custom()$nacho,
+      sample_ID, CartridgeID, BD, FoV, PCL, LoD, MC, MedC,
+      Positive_factor, House_factor
+    )
     filter(df,
-      BD < min(ot[["BD"]]) |
-      BD > max(ot[["BD"]]) |
+      BD < min(ot[["BD"]]) | BD > max(ot[["BD"]]) |
       FoV < ot[["FoV"]] |
       PCL < ot[["PCL"]] |
-      LoD < ot[["LoD"]]
+      LoD < ot[["LoD"]] |
+      Positive_factor < min(ot[["Positive_factor"]]) | Positive_factor > max(ot[["Positive_factor"]]) |
+      House_factor < min(ot[["House_factor"]]) | House_factor > max(ot[["House_factor"]])
+    )
+  })
+  output[["outliers"]] <- renderTable({ outliers_list() })
+  observe({
+    req(nrow(outliers_list()) != 0)
+    insertTab("main-menu",
+      tab = {
+        tabPanel(title = "Outliers", value = "outliers-tab",
+          fluidRow(column(width = 12, align = "center", tableOutput("outliers")))
+        )
+      },
+      target = "about-tab",
+      position = "before"
     )
   })
 }
