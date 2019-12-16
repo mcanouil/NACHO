0a2b6f4f0725c79c6e8f922a9376ef5516e3371e
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-12-16 17:49:49 +0100

    use check_outliers to annotate

--- R/normalise.R
+++ R/normalise.R
@@ -163,17 +163,7 @@ normalise <- function(
 
   if (remove_outliers & !nacho_object[["remove_outliers"]]) {
     nacho_object[["outliers_thresholds"]] <- outliers_thresholds
-    ot <- outliers_thresholds
-    nacho_object[["nacho"]][, "is_outlier"] <- {
-      nacho_object[["nacho"]][, "BD"] < min(ot[["BD"]]) | nacho_object[["nacho"]][, "BD"] > max(ot[["BD"]]) |
-      nacho_object[["nacho"]][, "FoV"] < ot[["FoV"]] |
-      nacho_object[["nacho"]][, "PCL"] < ot[["PCL"]] |
-      nacho_object[["nacho"]][, "LoD"] < ot[["LoD"]] |
-      nacho_object[["nacho"]][, "Positive_factor"] < min(ot[["Positive_factor"]]) |
-        nacho_object[["nacho"]][, "Positive_factor"] > max(ot[["Positive_factor"]]) |
-      nacho_object[["nacho"]][, "House_factor"] < min(ot[["House_factor"]]) |
-        nacho_object[["nacho"]][, "House_factor"] > max(ot[["House_factor"]])
-    }
+    nacho_object <- check_outliers(nacho_object)
 
     if (any(nacho_object[["nacho"]][, "is_outlier"]) | any(params_changed)) {
       nacho_object <- qc_rcc(
