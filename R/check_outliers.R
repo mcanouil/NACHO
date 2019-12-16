a96363a7fc11a61bef9e70256a75e15b8cc05848
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-12-16 18:00:57 +0100

    check attributes to filter on available metrics

--- R/check_outliers.R
+++ R/check_outliers.R
@@ -7,15 +7,26 @@
 #' @return [[list]] A list containing parameters and data.#'
 check_outliers <- function(nacho_object) {
   ot <- nacho_object[["outliers_thresholds"]]
-  nacho_object[["nacho"]][, "is_outlier"] <- {
-    nacho_object[["nacho"]][, "BD"] < min(ot[["BD"]]) | nacho_object[["nacho"]][, "BD"] > max(ot[["BD"]]) |
-    nacho_object[["nacho"]][, "FoV"] < ot[["FoV"]] |
-    nacho_object[["nacho"]][, "PCL"] < ot[["PCL"]] |
-    nacho_object[["nacho"]][, "LoD"] < ot[["LoD"]] |
-    nacho_object[["nacho"]][, "Positive_factor"] < min(ot[["Positive_factor"]]) |
-      nacho_object[["nacho"]][, "Positive_factor"] > max(ot[["Positive_factor"]]) |
-    nacho_object[["nacho"]][, "House_factor"] < min(ot[["House_factor"]]) |
-      nacho_object[["nacho"]][, "House_factor"] > max(ot[["House_factor"]])
+  if (attr(nacho_object, "RCC_type") == "n1") {
+    nacho_object[["nacho"]][, "is_outlier"] <- {
+      nacho_object[["nacho"]][, "BD"] < min(ot[["BD"]]) | nacho_object[["nacho"]][, "BD"] > max(ot[["BD"]]) |
+      nacho_object[["nacho"]][, "FoV"] < ot[["FoV"]] |
+      nacho_object[["nacho"]][, "PCL"] < ot[["PCL"]] |
+      nacho_object[["nacho"]][, "LoD"] < ot[["LoD"]] |
+      nacho_object[["nacho"]][, "Positive_factor"] < min(ot[["Positive_factor"]]) |
+        nacho_object[["nacho"]][, "Positive_factor"] > max(ot[["Positive_factor"]]) |
+      nacho_object[["nacho"]][, "House_factor"] < min(ot[["House_factor"]]) |
+        nacho_object[["nacho"]][, "House_factor"] > max(ot[["House_factor"]])
+    }
+  } else {
+    nacho_object[["nacho"]][, "is_outlier"] <- {
+      nacho_object[["nacho"]][, "BD"] < min(ot[["BD"]]) | nacho_object[["nacho"]][, "BD"] > max(ot[["BD"]]) |
+      nacho_object[["nacho"]][, "FoV"] < ot[["FoV"]] |
+      nacho_object[["nacho"]][, "Positive_factor"] < min(ot[["Positive_factor"]]) |
+        nacho_object[["nacho"]][, "Positive_factor"] > max(ot[["Positive_factor"]]) |
+      nacho_object[["nacho"]][, "House_factor"] < min(ot[["House_factor"]]) |
+        nacho_object[["nacho"]][, "House_factor"] > max(ot[["House_factor"]])
+    }
   }
   nacho_object
 }
