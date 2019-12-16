e61ea23a93f8fab09c0a8c1776bbdcddbadac8a7
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-12-16 17:53:27 +0100

    use check_outliers

--- R/summarise.R
+++ R/summarise.R
@@ -191,16 +191,7 @@ summarise <- function(
     House_factor = c(1/11, 11)
   )
   nacho_object[["outliers_thresholds"]] <- ot
-  nacho_object[["nacho"]][, "is_outlier"] <- {
-    nacho_object[["nacho"]][, "BD"] < min(ot[["BD"]]) | nacho_object[["nacho"]][, "BD"] > max(ot[["BD"]]) |
-    nacho_object[["nacho"]][, "FoV"] < ot[["FoV"]] |
-    nacho_object[["nacho"]][, "PCL"] < ot[["PCL"]] |
-    nacho_object[["nacho"]][, "LoD"] < ot[["LoD"]] |
-    nacho_object[["nacho"]][, "Positive_factor"] < min(ot[["Positive_factor"]]) |
-      nacho_object[["nacho"]][, "Positive_factor"] > max(ot[["Positive_factor"]]) |
-    nacho_object[["nacho"]][, "House_factor"] < min(ot[["House_factor"]]) |
-      nacho_object[["nacho"]][, "House_factor"] > max(ot[["House_factor"]])
-  }
+  nacho_object <- check_outliers(nacho_object)
 
   message(
     paste0(
