0ba26a12daef9a817b3311a8466058d4e76aa551
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-12-17 14:25:24 +0100

    add main upload page; add server side download for plot

--- inst/app/app.R
+++ inst/app/app.R
@@ -10,93 +10,158 @@ source("utils.R")
 
 ui <- tagList(
   tags$head(tags$style(HTML(
-    ".navbar-nav { float: none !important; } .navbar-nav > li:nth-child(6) { float: right; }"
+    ".navbar-nav { float: none !important; } .navbar-nav > li:nth-child(7) { float: right; }"
   ))),
   navbarPage(
-  theme = "united-bootstrap.min.css",
-  title = span(img(src = "nacho_hex.png", height = 18), "NACHO"),
-  windowTitle = "NACHO",
-  collapsible = TRUE,
-  id = "main-menu",
-  # footer = div(
-  #   p(
-  #     img(src = "www/nacho_hex.png", height = 18),
-  #     "Full documentation on", icon("github"), "at",
-  #     a(url = "https://mcanouil.github.io/NACHO", "mcanouil.github.io/NACHO")
-  #   ),
-  #   align = "center",
-  #   style = "margin: 5px;"
-  # ),
-  panelInputUI("qc_metrics", "QC Metrics",
-    sidebar = {
-      list(
-        h3("QC Thresholds"),
-        radioButtons("qc_bd_metrics", span("Binding Density", actionLink("about_bd", NULL, icon = icon("info-circle"))),
-          choiceNames = list(
-            span("MAX/FLEX", helpText("(Default: 0.1 - 2.25)")),
-            span("SPRINT", helpText("(Default: 0.1 - 1.8)"))
+    theme = "united-bootstrap.min.css",
+    title = tags$span(tags$img(src = "nacho_hex.png", height = 18), "NACHO"),
+    windowTitle = "NACHO",
+    collapsible = TRUE,
+    id = "main-menu",
+    # footer = tags$div(
+    #   tags$p(
+    #     tags$img(src = "www/nacho_hex.png", height = 18),
+    #     "Full documentation on", icon("github"), "at",
+    #     tags$a(url = "https://mcanouil.github.io/NACHO", "mcanouil.github.io/NACHO")
+    #   ),
+    #   align = "center",
+    #   style = "margin: 5px;"
+    # ),
+    selected = "qc_metrics",
+    tabPanel("Upload RCC Files", icon = icon("file-upload"), value = "upload-tab",
+      fluidRow(
+        column(width = 12,
+          card(title = "Upload RCC Files", body = {
+            fileInput("rcc_files", "Choose one or several RCC File",
+              multiple = TRUE,
+              accept = ".RCC"
+            )
+          })
+        )
+      ),
+      uiOutput("upload_ui")
+    ),
+    panelInputUI("qc_metrics", "QC Metrics",
+      sidebar = {
+        list(
+          h3("QC Thresholds"),
+          radioButtons("qc_bd_metrics", span("Binding Density", actionLink("about_bd", NULL, icon = icon("info-circle"))),
+            choiceNames = list(
+              span("MAX/FLEX", helpText("(Default: 0.1 - 2.25)")),
+              span("SPRINT", helpText("(Default: 0.1 - 1.8)"))
+            ),
+            choiceValues = list("MAX/FLEX", "SPRINT"),
+            inline = TRUE
           ),
-          choiceValues = list("MAX/FLEX", "SPRINT"),
-          inline = TRUE
-        ),
-        sliderInput("qc_bd_thresh", NULL,
-          min = 0.1, max = 2.25, value = c(0.1, 2.25)
-        ),
-        sliderInput("qc_fov_thresh", span("Field of View", actionLink("about_fov", NULL, icon = icon("info-circle")), helpText("(Default: 75)")),
-          min = 50, max = 100, value = 75
-        ),
-        sliderInput("qc_pcl_thresh", span("Positive Control Linearity", actionLink("about_pcl", NULL, icon = icon("info-circle")), helpText("(Default: 0.95)")),
-          min = 0.5, max = 1, value = 0.95
-        ),
-        sliderInput("qc_lod_thresh", span("Limit of Detection", actionLink("about_lod", NULL, icon = icon("info-circle")), helpText("(Default: 2)")),
-          min = 0, max = 30, value = 2
+          sliderInput("qc_bd_thresh", NULL,
+            min = 0.1, max = 2.25, value = c(0.1, 2.25)
+          ),
+          sliderInput("qc_fov_thresh", span("Field of View", actionLink("about_fov", NULL, icon = icon("info-circle")), helpText("(Default: 75)")),
+            min = 50, max = 100, value = 75
+          ),
+          sliderInput("qc_pcl_thresh", span("Positive Control Linearity", actionLink("about_pcl", NULL, icon = icon("info-circle")), helpText("(Default: 0.95)")),
+            min = 0.5, max = 1, value = 0.95
+          ),
+          sliderInput("qc_lod_thresh", span("Limit of Detection", actionLink("about_lod", NULL, icon = icon("info-circle")), helpText("(Default: 2)")),
+            min = 0, max = 30, value = 2
+          )
         )
-      )
-    },
-    plotInputUI("Binding Density"),
-    plotInputUI("Field of View", right = TRUE),
-    plotInputUI("Positive Control Linearity"),
-    plotInputUI("Limit of Detection", right = TRUE)
-  ),
-  panelInputUI("qc_control", "Control Probes",
-    sidebar = NULL,
-    plotInputUI("Positive Probes"),
-    plotInputUI("Negative Probes", right = TRUE),
-    plotInputUI("Housekeeping Genes Probes"),
-    plotInputUI("Control Probe Expression", right = TRUE)
-  ),
-  panelInputUI("qc_count", "Counts",
-    sidebar = NULL,
-    plotInputUI("Average Count vs. Binding Density"),
-    plotInputUI("Average Count vs. Median Count", right = TRUE),
-    plotInputUI("Principal Component Analysis"),
-    plotInputUI("Principal Component Analysis Inertia", right = TRUE)
-  ),
-  panelInputUI("norm", "Normalisation",
-    sidebar = {
-      list(
-        sliderInput("qc_pf_thresh", span("Positive Factor", helpText("(Default: 0.25 - 4)")),
-          min = 0.25, max = 4, value = c(0.25, 4)
-        ),
-        sliderInput("qc_hgf_thresh", span("Housekeeping Genes Factor", helpText("(Default: 0.09 - 11)")),
-          min = 0.09, max = 11, value = c(0.09, 11)
+      },
+      plotInputUI("Binding Density"),
+      plotInputUI("Field of View", right = TRUE),
+      plotInputUI("Positive Control Linearity"),
+      plotInputUI("Limit of Detection", right = TRUE)
+    ),
+    panelInputUI("qc_control", "Control Probes",
+      sidebar = NULL,
+      plotInputUI("Positive Probes"),
+      plotInputUI("Negative Probes", right = TRUE),
+      plotInputUI("Housekeeping Genes Probes"),
+      plotInputUI("Control Probe Expression", right = TRUE)
+    ),
+    panelInputUI("qc_count", "Counts",
+      sidebar = NULL,
+      plotInputUI("Average Count vs. Binding Density"),
+      plotInputUI("Average Count vs. Median Count", right = TRUE),
+      plotInputUI("Principal Component Analysis"),
+      plotInputUI("Principal Component Analysis Inertia", right = TRUE)
+    ),
+    panelInputUI("norm", "Normalisation",
+      sidebar = {
+        list(
+          sliderInput("qc_pf_thresh", span("Positive Factor", helpText("(Default: 0.25 - 4)")),
+            min = 0.25, max = 4, value = c(0.25, 4)
+          ),
+          sliderInput("qc_hgf_thresh", span("Housekeeping Genes Factor", helpText("(Default: 0.09 - 11)")),
+            min = 0.09, max = 11, value = c(0.09, 11)
+          )
         )
-      )
-    },
-    plotInputUI("Positive Factor vs. Negative Factor"),
-    plotInputUI("Housekeeeping Genes Factor", right = TRUE),
-    plotInputUI("Normalisation Result", right = TRUE)
-  ),
-  tabPanel(title = "Outliers", value = "outliers-tab",
-    card(title = h4("Outliers List"), list(uiOutput("outliers-thresholds"), tableOutput("outliers")))
-  ),
-  tabPanel("About", icon = icon("info"), value = "about-tab",
-    p(includeMarkdown("www/about-nacho.md"))
+      },
+      plotInputUI("Positive Factor vs. Negative Factor"),
+      plotInputUI("Housekeeeping Genes Factor", right = TRUE),
+      plotInputUI("Normalisation Result", right = TRUE)
+    ),
+    tabPanel(title = "Outliers", value = "outliers-tab",
+      card(title = h4("Outliers List"), list(uiOutput("outliers-thresholds"), tableOutput("outliers")))
+    ),
+    tabPanel("About", icon = icon("info"), value = "about-tab",
+      p(includeMarkdown("www/about-nacho.md"))
+    )
   )
-))
+)
+
+nacho_object <- get(data(GSE74821, package = "NACHO"))
 
 server <- function(input, output, session) {
-  nacho_object <- reactive({ get(data(GSE74821, package = "NACHO")) })
+  # ---------------------------------------- Upload
+  observe({
+    if (exists("nacho_object")) {
+      hideTab("main-menu", target = "upload-tab")
+    } else {
+      showTab("main-menu", target = "upload-tab", select = TRUE)
+      map(
+        .x = paste0(c("qc_metrics", "qc_control", "qc_count", "norm", "outliers"), "-tab"),
+        .f = ~ hideTab("main-menu", target = .x)
+      )
+    }
+  })
+  nacho_react <- reactive({
+    req(exists("nacho_object"))
+    nacho_object
+  })
+
+  output$rcc_contents <- renderTable({ req(input$rcc_files) })
+  output$rcc_contents_summary <- renderUI({
+    rcc_size <- sum(input$rcc_files[, "size"])
+    class(rcc_size) <- "object_size"
+    tags$p(
+      "A total of", tags$strong(length(input$rcc_files[, "name"])), "RCC files were succesfully uploaded,",
+      "for a total amount of", tags$strong(format(rcc_size, units = "Mb")), "."
+    )
+  })
+  output$upload_ui <- renderUI({
+    if (is.null(input$rcc_files)) {
+      fluidRow(style = "padding-top: 1em;",
+        column(width = 12,
+          card(title = "Summary", body = tags$p("No RCC files uploaded."))
+        )
+      )
+    } else {
+      list(
+        fluidRow(style = "padding-top: 1em;",
+          column(width = 12,
+            card(title = "Summary", body = uiOutput("rcc_contents_summary"))
+          )
+        ),
+        fluidRow(style = "padding-top: 1em;",
+          column(width = 12,
+            card(title = "RCC Files Uploaded", body = tableOutput("rcc_contents"))
+          )
+        )
+      )
+    }
+  })
+
   # ---------------------------------------- UI / SERVER
   # Global UI input
   observe({
@@ -118,14 +183,12 @@ server <- function(input, output, session) {
     switch(req(input$qc_bd_metrics),
       "MAX/FLEX" = {
         updateSliderInput(session, "qc_bd_thresh",
-          max = 2.25,
-          value = isolate(input$qc_bd_thresh)
+          max = 2.25, value = isolate(input$qc_bd_thresh)
         )
       },
       "SPRINT" = {
         updateSliderInput(session, "qc_bd_thresh",
-          max = 1.8,
-          value = isolate(input$qc_bd_thresh)
+          max = 1.8, value = isolate(input$qc_bd_thresh)
         )
       }
     )
@@ -158,20 +221,22 @@ server <- function(input, output, session) {
   # ---------------------------------------- Input
   # Get nacho object and update thresholds
   nacho_custom <- reactive({
-    nacho_object <- nacho_object()
-    nacho_object$outliers_thresholds[["BD"]] <- input$qc_bd_thresh %||%
-      nacho_object$outliers_thresholds[["BD"]]
-    nacho_object$outliers_thresholds[["FoV"]] <- input$qc_fov_thresh %||%
-      nacho_object$outliers_thresholds[["FoV"]]
-    nacho_object$outliers_thresholds[["LoD"]] <- input$qc_lod_thresh %||%
-      nacho_object$outliers_thresholds[["LoD"]]
-    nacho_object$outliers_thresholds[["PCL"]] <- input$qc_pcl_thresh %||%
-      nacho_object$outliers_thresholds[["PCL"]]
-    nacho_object$outliers_thresholds[["Positive_factor"]] <- input$qc_pf_thresh %||%
-      nacho_object$outliers_thresholds[["Positive_factor"]]
-    nacho_object$outliers_thresholds[["House_factor"]] <- input$qc_hgf_thresh %||%
-      nacho_object$outliers_thresholds[["House_factor"]]
-    check_outliers(nacho_object)
+    nacho <- req(nacho_react())
+
+    nacho$outliers_thresholds[["BD"]] <- input$qc_bd_thresh %||%
+      nacho$outliers_thresholds[["BD"]]
+    nacho$outliers_thresholds[["FoV"]] <- input$qc_fov_thresh %||%
+      nacho$outliers_thresholds[["FoV"]]
+    nacho$outliers_thresholds[["LoD"]] <- input$qc_lod_thresh %||%
+      nacho$outliers_thresholds[["LoD"]]
+    nacho$outliers_thresholds[["PCL"]] <- input$qc_pcl_thresh %||%
+      nacho$outliers_thresholds[["PCL"]]
+    nacho$outliers_thresholds[["Positive_factor"]] <- input$qc_pf_thresh %||%
+      nacho$outliers_thresholds[["Positive_factor"]]
+    nacho$outliers_thresholds[["House_factor"]] <- input$qc_hgf_thresh %||%
+      nacho$outliers_thresholds[["House_factor"]]
+
+    check_outliers(nacho)
   })
 
   # ---------------------------------------- Output
