4b7a8a38ba85511f38749210167ba1010da3c7fb
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-12-18 14:11:03 +0100

    remove unused code

--- R/geometric_housekeeping.R
+++ R/geometric_housekeeping.R
@@ -11,11 +11,11 @@
 #' @return [[numeric]]
 geometric_housekeeping <- function(data, positive_factor, intercept, housekeeping_genes) {
   house_data <- as.data.frame(data[, c("Name", "CodeClass", "Count")])
-  if (is.null(housekeeping_genes)) {
-    house_data <- house_data[house_data[["CodeClass"]] %in% "Housekeeping", ]
-  } else {
-    house_data <- house_data[house_data[["Name"]] %in% housekeeping_genes, ]
-  }
+  # if (is.null(housekeeping_genes)) {
+  #   house_data <- house_data[house_data[["CodeClass"]] %in% "Housekeeping", ]
+  # } else {
+  house_data <- house_data[house_data[["Name"]] %in% housekeeping_genes, ]
+  # }
 
   house_data[["Count"]] <- house_data[["Count"]] - intercept
   house_data[["Count"]] <- house_data[["Count"]] * positive_factor
