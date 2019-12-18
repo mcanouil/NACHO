a5b8bb971fb95cb090f8c16dbf20c19ae569c3ac
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-12-18 14:08:45 +0100

    new deploy function

---
+++ R/deploy.R
@@ -0,0 +1,24 @@
+#' Deploy (copy) the shiny application to the specified directory.
+#'
+#' @param directory [[character]] A character vector of one path to the new location.
+#' @param app_name [[character]] A character vector defining the shiny application name in the new location.
+#'
+#' @return [[logical]] A logical indicating whether the deployment is successfull (`TRUE`) or not (`FALSE`).
+#' @export
+#'
+#' @examples
+#'
+#' deploy(directory = ".")
+#'
+#' if (interactive()) {
+#'   shiny::runApp("NACHO")
+#' }
+#'
+deploy <- function(directory, app_name = "NACHO") {
+  dir.create(file.path(directory, app_name), showWarnings = FALSE, recursive = TRUE)
+  all(file.copy(
+    from = list.files(system.file("app", package = "NACHO"), full.names = TRUE),
+    to = file.path(directory, app_name),
+    overwrite = TRUE, recursive = TRUE
+  ))
+}
