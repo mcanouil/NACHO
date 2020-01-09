5e6881c312cc0ffb549e403b14582946d413594b
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-01-09 16:30:10 +0100

    remove manual checks

--- .cran-checks.R
+++ .cran-checks.R
@@ -1,59 +1,27 @@
 devtools::check()
-
 devtools::check_win_devel()
-
 cran <- rhub::check_for_cran()
 sol <- rhub::check_on_solaris()
 mac <- rhub::check_on_macos()
 
-# cran_prep <- rhub::check_for_cran(
-#   check_args = "'--as-cran --no-manual --no-build-vignettes'",
-#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false")
-# )
-
 # cran_prep <- rhub::check(
 #   platform = c(
-#     # "debian-clang-devel",
-#     # "debian-gcc-devel",
-#     # "debian-gcc-patched",
-#     # "debian-gcc-release",
+#     "debian-clang-devel",
+#     "debian-gcc-devel",
+#     "debian-gcc-patched",
+#     "debian-gcc-release",
 #     "fedora-clang-devel",
-#     # "fedora-gcc-devel",
+#     "fedora-gcc-devel",
 #     "windows-x86_64-devel",
-#     # "solaris-x86-patched",
+#     "solaris-x86-patched",
 #     "windows-x86_64-release",
 #     "macos-elcapitan-release"
 #   ),
-#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
-#   check_args = "'--as-cran'",
-#   show_status = FALSE
-# )
-# cran_prep <- rhub::check(
-#   platform = c(
-#     # "debian-clang-devel",
-#     # "debian-gcc-devel",
-#     # "debian-gcc-patched",
-#     # "debian-gcc-release",
-#     "fedora-clang-devel",
-#     # "fedora-gcc-devel",
-#     "windows-x86_64-devel",
-#     # "solaris-x86-patched",
-#     "windows-x86_64-release",
-#     "macos-elcapitan-release"
-#   ),
-#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
-#   check_args = "'--as-cran'",
-#   show_status = FALSE
-# )
-# cran_prep_sol <- rhub::check(
-#   platform = "solaris-x86-patched",
 #   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
 #   check_args = "'--as-cran --no-manual --no-build-vignettes'",
 #   show_status = FALSE
 # )
 
 # rm CRAN-RELEASE
-
 # git tag v1.0.0
-
 # usethis::use_dev_version()
