377e3d7cb759a623bb3b6d671c574166bc31409f
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-01-09 14:24:28 +0100

    rename scripts for cran checks

--- .cran-checks.R
+++ .cran-checks.R
@@ -2,38 +2,38 @@ devtools::check()
 
 devtools::check_win_devel()
 
+cran_prep <- rhub::check_for_cran()
 # cran_prep <- rhub::check_for_cran(
 #   check_args = "'--as-cran --no-manual --no-build-vignettes'",
 #   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false")
 # )
 
-cran_prep <- rhub::check(
-  platform = c(
-    "debian-clang-devel",
-    "debian-gcc-devel",
-    "debian-gcc-patched",
-    "debian-gcc-release",
-    "fedora-clang-devel",
-    "fedora-gcc-devel",
-    "windows-x86_64-devel",
-    # "solaris-x86-patched",
-    "windows-x86_64-release",
-    "macos-elcapitan-release"
-  ),
-  env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
-  check_args = "'--as-cran'",
-  show_status = FALSE
-)
-cran_prep_sol <- rhub::check(
-  platform = "solaris-x86-patched",
-  env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
-  check_args = "'--as-cran --no-manual --no-build-vignettes'",
-  show_status = FALSE
-)
+# cran_prep <- rhub::check(
+#   platform = c(
+#     # "debian-clang-devel",
+#     # "debian-gcc-devel",
+#     # "debian-gcc-patched",
+#     # "debian-gcc-release",
+#     "fedora-clang-devel",
+#     # "fedora-gcc-devel",
+#     "windows-x86_64-devel",
+#     # "solaris-x86-patched",
+#     "windows-x86_64-release",
+#     "macos-elcapitan-release"
+#   ),
+#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
+#   check_args = "'--as-cran'",
+#   show_status = FALSE
+# )
+# cran_prep_sol <- rhub::check(
+#   platform = "solaris-x86-patched",
+#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
+#   check_args = "'--as-cran --no-manual --no-build-vignettes'",
+#   show_status = FALSE
+# )
 
-# rhub::list_package_checks()
-
-cran_prep$cran_summary()
+saveRDS(cran_prep, "../../cran_prep.rds")
+# cran_prep$cran_summary()
 
 # rm CRAN-RELEASE
 
