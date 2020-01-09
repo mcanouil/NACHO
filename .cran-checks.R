0a978c712419e6678ce33e4b4acd43139ca76fdf
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-01-09 16:29:54 +0100

    simplify cran checks

--- .cran-checks.R
+++ .cran-checks.R
@@ -2,12 +2,32 @@ devtools::check()
 
 devtools::check_win_devel()
 
-cran_prep <- rhub::check_for_cran()
+cran <- rhub::check_for_cran()
+sol <- rhub::check_on_solaris()
+mac <- rhub::check_on_macos()
+
 # cran_prep <- rhub::check_for_cran(
 #   check_args = "'--as-cran --no-manual --no-build-vignettes'",
 #   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false")
 # )
 
+# cran_prep <- rhub::check(
+#   platform = c(
+#     # "debian-clang-devel",
+#     # "debian-gcc-devel",
+#     # "debian-gcc-patched",
+#     # "debian-gcc-release",
+#     "fedora-clang-devel",
+#     # "fedora-gcc-devel",
+#     "windows-x86_64-devel",
+#     # "solaris-x86-patched",
+#     "windows-x86_64-release",
+#     "macos-elcapitan-release"
+#   ),
+#   env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",  `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"),
+#   check_args = "'--as-cran'",
+#   show_status = FALSE
+# )
 # cran_prep <- rhub::check(
 #   platform = c(
 #     # "debian-clang-devel",
@@ -31,9 +51,6 @@ cran_prep <- rhub::check_for_cran()
 #   check_args = "'--as-cran --no-manual --no-build-vignettes'",
 #   show_status = FALSE
 # )
-
-saveRDS(cran_prep, "../../cran_prep.rds")
-# cran_prep$cran_summary()
 
 # rm CRAN-RELEASE
 
