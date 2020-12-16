a28f3ded24a3f08cd44375a02f50ca9912371e98
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-12-16 13:57:32 +0100

    add spaces

--- R/format_tag_content.R
+++ R/format_tag_content.R
@@ -8,7 +8,7 @@
 #'
 #' @return [data.frame]
 format_tag_content <- function(tag, content) {
-  if (nrow(content)==1 & is.na(content[1, 1])) {
+  if (nrow(content) == 1 & is.na(content[1, 1])) {
     output <- content
   } else {
     output <- as.data.frame(x = t(content[, -1, drop = FALSE]), stringsAsFactors = FALSE)
