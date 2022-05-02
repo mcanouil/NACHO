ecd16fa2ee28ab1025cbc2afbc8d6dbcf5afe191
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-02 22:45:44 +0200

    style: rm curly braces

--- R/probe_exclusion.R
+++ R/probe_exclusion.R
@@ -12,7 +12,7 @@ probe_exclusion <- function(control_genes_df) {
   local_neg <- control_genes_df[control_genes_df[["CodeClass"]] %in% "Negative", count_columns]
   overal_median <- stats::median(as.matrix(local_neg))
   medians <- apply(X = local_neg, MARGIN = 1, FUN = stats::median)
-  delta_medians <- sapply(medians, function(x) {abs((overal_median - x))})
+  delta_medians <- sapply(medians, function(x) abs((overal_median - x)))
   ex_probes <- delta_medians[delta_medians > (0.5 * overal_median)]
   if (length(ex_probes) != 0 && nrow(local_neg) != length(ex_probes)) {
     names(ex_probes)
