4ff6ce65586df38347a5000cf7b9bd4e8acdf806
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-30 21:02:32 +0200

    fix: copy/paste type

--- tests/testthat.R
+++ tests/testthat.R
@@ -20,10 +20,10 @@ plexset_nacho <- load_rcc(
 rcc_files_directory <- "testthat/salmon_data"
 salmon_files <- list.files(rcc_files_directory, full.names = TRUE, pattern = "\\.RCC")
 salmon_tidy <- data.frame(stringsAsFactors = FALSE,
-  name = basename(plexset_files),
-  datapath = plexset_files,
-  IDFILE = basename(plexset_files),
-  plexset_id = rep(paste0("S", seq_len(8)), each = length(plexset_files))
+  name = basename(salmon_files),
+  datapath = salmon_files,
+  IDFILE = basename(salmon_files),
+  plexset_id = rep(paste0("S", seq_len(8)), each = length(salmon_files))
 )
 salmon_nacho <- load_rcc(
   data_directory = rcc_files_directory,
