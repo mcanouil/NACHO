e0defe4003074d7cef15d98eb46ba84400d01a41
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-05-30 20:59:09 +0200

    fix: only search for RCC files

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,11 +4,12 @@ library(NACHO)
 # setwd("tests")
 
 rcc_files_directory <- "testthat/plexset_data"
+plexset_files <- list.files(rcc_files_directory, full.names = TRUE, pattern = "\\.RCC")
 plexset_tidy <- data.frame(stringsAsFactors = FALSE,
-  name = list.files(rcc_files_directory),
-  datapath = list.files(rcc_files_directory, full.names = TRUE),
-  IDFILE = basename(list.files(rcc_files_directory, full.names = TRUE)),
-  plexset_id = rep(paste0("S", seq_len(8)), each = length(list.files(rcc_files_directory)))
+  name = basename(plexset_files),
+  datapath = plexset_files,
+  IDFILE = basename(plexset_files),
+  plexset_id = rep(paste0("S", seq_len(8)), each = length(plexset_files))
 )
 plexset_nacho <- load_rcc(
   data_directory = rcc_files_directory,
@@ -17,11 +18,12 @@ plexset_nacho <- load_rcc(
 )
 
 rcc_files_directory <- "testthat/salmon_data"
+salmon_files <- list.files(rcc_files_directory, full.names = TRUE, pattern = "\\.RCC")
 salmon_tidy <- data.frame(stringsAsFactors = FALSE,
-  name = list.files(rcc_files_directory),
-  datapath = list.files(rcc_files_directory, full.names = TRUE),
-  IDFILE = basename(list.files(rcc_files_directory, full.names = TRUE)),
-  plexset_id = rep(paste0("S", seq_len(8)), each = length(list.files(rcc_files_directory)))
+  name = basename(plexset_files),
+  datapath = plexset_files,
+  IDFILE = basename(plexset_files),
+  plexset_id = rep(paste0("S", seq_len(8)), each = length(plexset_files))
 )
 salmon_nacho <- load_rcc(
   data_directory = rcc_files_directory,
