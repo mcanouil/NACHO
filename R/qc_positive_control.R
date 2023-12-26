001f02d1d9fa2dc796dbc831895d69e7434e40ca
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2023-12-26 20:54:21 +0100

    fix: log zero values

--- R/qc_positive_control.R
+++ R/qc_positive_control.R
@@ -8,7 +8,11 @@
 #'
 #' @return [[numeric]]
 qc_positive_control <- function(counts) {
-  measured <- log2(counts[["Count"]])
+  if (any(counts[["Count"]] %in% 0)) {
+    measured <- log2(counts[["Count"]] + 1)
+  } else {
+    measured <- log2(counts[["Count"]])
+  }
   known <- log2(as.numeric(sub("^[^(]*\\((.*)\\)$", "\\1", counts[["Name"]]))) # plexset value: "32"
   correlation <- summary(stats::lm(measured ~ known))$r.squared
   unname(round(correlation, 5))
